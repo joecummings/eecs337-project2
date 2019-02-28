39ef1a545b0be240402eb79b504436a7d9e72bba
Jack Richard <richarjw@rose-hulman.edu>
2019-02-27 23:14:10 -0600

    Scraper now gets: Recipe Title, Rating, Reviews, Calorie Count, Ingredients, and Directions

--- query.py
+++ query.py
@@ -2,8 +2,10 @@
 import sys
 import requests
 from bs4 import BeautifulSoup
-import scraper
+import scraper as s
+import re
 query = ""
+
 
 def main():
     if len(sys.argv) != 2:
@@ -12,9 +14,74 @@ def main():
         query = sys.argv[1]
         print("Recipe Link: {0}".format(query))
         page = requests.get(query).text
-        soup = BeautifulSoup(page,'html.parser')
-        print(soup)
+        soup = BeautifulSoup(page, 'html.parser')
+        scrape_recipe(soup)
 
+
+def scrape_recipe(soup):
+        # This is called when user wants to scrape for specific recipe site
+        # Try functions were used to prevent any one element from stopping the operation
+
+    try:
+        rtitle = soup.find_all('h1')
+        rtitle = cleanhtml(str(rtitle[0]))
+    except:
+        rtitle = 'NA'
+
+    print("Recipe Title: {0}".format(rtitle))
+
+    try:
+        starrating = soup.find_all('div',{'class':'rating-stars'})
+        starrating = starrating[0]['data-ratingstars']
+    except:
+        starrating = 'NA'
+    print("Rating: {0} out of 5".format(starrating))
+
+    try:
+        reviewcount = soup.find_all("meta",{'itemprop':'reviewCount'})
+        reviewcount = reviewcount[0]['content']
+    except:
+        reviewcount = 'NA'
+
+    print("Number of Reviews: {0}".format(reviewcount))
+
+    try:
+        calcount = soup.find_all("span",{"class":'calorie-count'})
+        calcount = calcount[0]['aria-label']
+    except:
+        calcount = 'NA'
+
+    print("Calorie Count: {0}".format(calcount))
+
+    try: 
+        ingredients = soup.find_all("span", {"itemprop": "recipeIngredient"})
+        for i in ingredients:
+            ingredients[ingredients.index(i)] = cleanhtml(str(i))
+    except:
+        ingredients = "NA"
+
+    print("INGREDIENTS:")
+
+    for i in ingredients:
+        print(i) 
+
+    try:
+        directions = soup.find_all("span", {"class": "recipe-directions__list--item"})
+        for i in directions:
+            directions[directions.index(i)] = cleanhtml(str(i))
+    except:
+        directions = "NA"
+
+    print("DIRECTIONS")
+
+    for d in directions:
+        if d.strip() != "":
+            print("{0}) {1}".format(directions.index(d)+1,d))
+
+def cleanhtml(raw_html):
+  cleanr = re.compile('<.*?>')
+  cleantext = re.sub(cleanr, '', raw_html)
+  return cleantext.strip()
 
 if __name__ == "__main__":
     main()
--- scraper.py
+++ scraper.py
@@ -1,1 +1,66 @@
-#This file will contain all of the functions necessary to parse the beautifulsoup response text
+# This file will contain all of the functions necessary to parse the beautifulsoup response text
+def clean():
+    print("DO STUFF")
+
+def scrape_recipe(br, year, idnumber):
+    # This is called when user wants to scrape for specific recipe site
+    # Try functions were used to prevent any one element from stopping the operation
+
+    # recipe title
+    try:
+        rtitle = br.find_element_by_tag_name('h1').text
+    except:
+        rtitle = 'NA'
+
+    # Star rating
+    try:
+        starrating = br.find_element_by_class_name('rating-stars').\
+            get_attribute('data-ratingstars')
+    except:
+        starrating = 'NA'
+
+    # Number of people who clicked that they "made it"
+    try:
+        madeitcount = br.find_element_by_class_name('made-it-count').text
+    except:
+        madeitcount = 'NA'
+
+    # Number of reviews
+    try:
+        reviewcount = br.find_element_by_class_name('review-count').text
+        reviewcount = str(re.findall('(\w+) reviews', reviewcount)[0])
+    except:
+        reviewcount = 'NA'
+
+    # calories per serving
+    try:
+        calcount = br.find_element_by_class_name('calorie-count').text
+        calcount = str(re.findall('(\w+) cals', calcount)[0])
+    except:
+        calcount = 'NA'
+
+    # prep time
+    try:
+        prepTime = br.find_element_by_xpath('//time[@itemprop = "prepTime"]').\
+            get_attribute('datetime')
+        prepTime = str(re.findall('PT(\w+)', prepTime)[0])
+    except:
+        prepTime = 'NA'
+
+    # Cook time
+    try:
+        cookTime = br.find_element_by_xpath('//time[@itemprop = "cookTime"]').\
+            get_attribute('datetime')
+        cookTime = str(re.findall('PT(\w+)', cookTime)[0])
+    except:
+        cookTime = 'NA'
+
+    # total time
+    try:
+        totalTime = br.find_element_by_xpath('//time[@itemprop = "totalTime"]').\
+            get_attribute('datetime')
+        totalTime = str(re.findall('PT(\w+)', totalTime)[0])
+    except:
+        totalTime = 'NA'
+    # find all the ingredient attributes
+    ingred = br.find_elements_by_class_name("checkList__item")
